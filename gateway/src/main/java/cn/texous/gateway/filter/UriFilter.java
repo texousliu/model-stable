fe491e4185e9b166272119763756543e80d66bd6
liuxiaohua <504579863@qq.com>
2018-08-17 17:13:27 +0800

    add customer zuulfilter

---
+++ gateway/src/main/java/cn/texous/gateway/filter/UriFilter.java
@@ -0,0 +1,43 @@
+package cn.texous.gateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * insert description here
+ *
+ * @author liuxiaohua
+ * @since 2018/8/17 15:30
+ */
+@Component
+public class UriFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        System.out.println(request);
+        System.out.println(request.getRequestURI());
+        return null;
+    }
+}
